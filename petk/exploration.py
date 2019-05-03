7ceb7c5f904adb68f442beab33596c71e4d3212b
dottyz <yizhao.tan@gmail.com>
2019-05-03 14:27:56 -0400

    Property name change

--- petk/exploration.py
+++ petk/exploration.py
@@ -43,10 +43,10 @@ class DataReport:
         pass
 
 class DataProfile:
-    def __init__(self, array, top=5, visualize=True):
-        self.array = array
+    def __init__(self, series, top=5, visualize=True):
+        self.series = series
 
-        dtype = utils.get_type(self.array)
+        dtype = utils.get_type(self.series)
         # TODO: Convert these 2 functions to static functions
         desc = self.get_description()
         fv = self.get_frequent_values(top)
@@ -56,63 +56,63 @@ class DataProfile:
         self.frequent_values = fv
 
     def get_description(self):
-        count = self.array.count() # ONLY non-NaN observations
+        count = self.series.count() # ONLY non-NaN observations
 
         description = {
             'type': self.dtype,
-            'memory_usage': self.array.memory_usage(),
+            'memory_usage': self.series.memory_usage(),
             'count': count,
-            'p_missing': (self.array.size - count) / self.array.size,
-            'n_missing': self.array.size - count,
+            'p_missing': (self.series.size - count) / self.series.size,
+            'n_missing': self.series.size - count,
         }
 
         if not self.dtype in [constants.TYPE_UNSUPPORTED, constants.TYPE_CONST, constants.TYPE_UNIQUE]:
-            n_distinct = self.array.nunique()
+            n_distinct = self.series.nunique()
 
             description.update({
                 'distinct_count': distinct_count,
-                'is_unique': distinct_count == self.array.size,
-                'p_unique': distinct_count * 1.0 / self.array.size
+                'is_unique': distinct_count == self.series.size,
+                'p_unique': distinct_count * 1.0 / self.series.size
             })
 
             if self.dtype == constants.TYPE_BOOL:
                 description.update({
-                    'mean': self.array.mean()
+                    'mean': self.series.mean()
                 })
             elif self.dtype in [constants.TYPE_DATE, constants.TYPE_NUM]:
-                n_inf = self.array.loc[(~np.isfinite(self.array)) & self.array.notnull()].size
+                n_inf = self.series.loc[(~np.isfinite(self.series)) & self.series.notnull()].size
 
                 description.update({
-                    'p_infinite': n_inf / self.array.size,
+                    'p_infinite': n_inf / self.series.size,
                     'n_infinite': n_inf,
-                    'min': self.array.min(),
-                    'max': self.array.max()
+                    'min': self.series.min(),
+                    'max': self.series.max()
                 })
 
-                for perc in np.array([0.05, 0.25, 0.5, 0.75, 0.95]):
-                    description['{:.0%}'.format(perc)] = self.array.quantile(perc)
+                for perc in np.series([0.05, 0.25, 0.5, 0.75, 0.95]):
+                    description['{:.0%}'.format(perc)] = self.series.quantile(perc)
 
                 if self.dtype == constants.TYPE_NUM:
-                    n_zeros = self.array.size - np.count_nonzero(self.array)
+                    n_zeros = self.series.size - np.count_nonzero(self.series)
 
                     description.update({
-                        'mean': self.array.mean(),
-                        'std': self.array.std(),
-                        'variance': self.array.var(),
-                        'iqr': self.array.quantile(0.75) - self.array.quantile(0.25),
-                        'kurtosis': self.array.kurt(),
-                        'skewness': self.array.skew(),
-                        'sum': self.array.sum(),
-                        'mad': self.array.mad(),
-                        'cv': self.array.std() / self.array.mean(),
+                        'mean': self.series.mean(),
+                        'std': self.series.std(),
+                        'variance': self.series.var(),
+                        'iqr': self.series.quantile(0.75) - self.series.quantile(0.25),
+                        'kurtosis': self.series.kurt(),
+                        'skewness': self.series.skew(),
+                        'sum': self.series.sum(),
+                        'mad': self.series.mad(),
+                        'cv': self.series.std() / self.series.mean(),
                         'n_zeros': n_zeros,
-                        'p_zeros': n_zeros / self.array.size
+                        'p_zeros': n_zeros / self.series.size
                     })
 
         return description
 
     def get_frequent_values(self, top):
-        return self.array.value_counts(dropna=False).head(top)
+        return self.series.value_counts(dropna=False).head(top)
 
     def get_distribution(self, top):
         assert self.dtype in [constants.STR, constants.DATE, constants.NUM], 'Distribution not available'
