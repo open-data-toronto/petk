986a80f0079b196da2f1c2d8858603b761a9fb2b
dottyz <yizhao.tan@gmail.com>
2019-05-08 11:36:43 -0400

    GeoReport MVP and better ordering of DataReport structure

--- petk/explore.py
+++ petk/explore.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from shapely.validation import explain_validity
 
 import geopandas as gpd
@@ -12,20 +14,20 @@ import petk.utils as utils
 class DataReport:
     def __init__(self, data):
         self.df = data
+        self.df.index.name = 'index'
+
         self.description = {}
 
     @property
     def describe(self):
-        keys = []
-        for k, v in self.description.items():
-            if len(v.description.keys()) > len(keys):
-                keys = v.description.keys()
+        desc = pd.DataFrame(self.description.values())
 
-        return pd.DataFrame(
-            [x.description for x in self.description.values()],
-            columns=keys
-        ).set_index('column').T
+        if not desc.empty:
+            desc = desc.set_index('name')
 
+        return desc.T
+
+    @property
     def introduce(self):
         dd = pd.Series({
             'memory_usage': np.sum(self.df.memory_usage(deep=True)),
@@ -39,100 +41,137 @@ class DataReport:
             'columns: {0}'.format(utils.get_type(self.df[col]).lower()) for col in self.df.columns
         ]).value_counts()
 
-        return dd.append(cd)
+        return dd.append(cd).to_frame(name='values')
 
     def profile_columns(self, columns=[]):
-        # TODO: assert kwargs
-
         if not columns:
             columns = self.df.columns
+        elif not isinstance(columns, list):
+            columns = [columns]
 
-        for col in columns:
-            if col not in self.description.keys():
-                self.description[col] = get_description(self.df[col])
+        miss = [x for x in columns if not x in self.df.columns]
+        assert not miss, 'Columns "{0}" not in data'.format(', '.join(miss))
 
-    def get_description(self, series):
-        count = self.series.count() # ONLY non-NaN observations
+        for c in columns:
+            if c not in self.description.keys():
+                self.description[c] = self.get_description(c)
 
-        description = {
-            'type': self.dtype,
-            'column': self.series.name,
-            'memory_usage': self.series.memory_usage(),
-            'count': count,
-            'p_missing': (self.series.size - count) / self.series.size,
-            'n_missing': self.series.size - count,
-        }
+    def get_description(self, column):
+        series = self.df[column]
 
-        if not self.dtype in [constants.TYPE_UNSUPPORTED, constants.TYPE_CONST, constants.TYPE_UNIQUE]:
-            n_distinct = self.series.nunique()
+        count = series.count() # ONLY non-NaN observations
+        dtype = utils.get_type(series)
 
-            description.update({
-                'distinct_count': n_distinct,
-                'is_unique': n_distinct == self.series.size,
-                'p_unique': n_distinct * 1.0 / self.series.size
-            })
+        description = [
+            ('type', dtype),
+            ('name', series.name),
+            ('memory_usage', series.memory_usage()),
+            ('count', count),
+            ('p_missing', (series.size - count) / series.size),
+            ('n_missing', series.size - count),
+        ]
 
-            if self.dtype == constants.TYPE_BOOL:
-                description.update({
-                    'mean': self.series.mean()
-                })
-            elif self.dtype in [constants.TYPE_DATE, constants.TYPE_NUM]:
-                n_inf = self.series.loc[(~np.isfinite(self.series)) & self.series.notnull()].size
+        if not dtype in [constants.TYPE_UNSUPPORTED, constants.TYPE_CONST, constants.TYPE_UNIQUE]:
+            n_distinct = series.nunique()
 
-                description.update({
-                    'p_infinite': n_inf / self.series.size,
-                    'n_infinite': n_inf,
-                    'min': self.series.min(),
-                    'max': self.series.max()
-                })
+            description += [
+                ('distinct_count', n_distinct),
+                ('is_unique', n_distinct == series.size),
+                ('p_unique', n_distinct * 1.0 / series.size)
+            ]
 
-                for perc in [0.05, 0.25, 0.5, 0.75, 0.95]:
-                    description['{:.0%}'.format(perc)] = self.series.quantile(perc)
+            if dtype == constants.TYPE_BOOL:
+                description += [
+                    ('mean', series.mean())
+                ]
+            elif dtype in [constants.TYPE_DATE, constants.TYPE_NUM]:
+                n_inf = series.loc[(~np.isfinite(series)) & series.notnull()].size
 
-                if self.dtype == constants.TYPE_NUM:
-                    n_zeros = self.series.size - np.count_nonzero(self.series)
+                description += [
+                    ('p_infinite', n_inf / series.size),
+                    ('n_infinite', n_inf),
+                    ('min', series.min()),
+                    ('max', series.max())
+                ]
 
-                    description.update({
-                        'mean': self.series.mean(),
-                        'std': self.series.std(),
-                        'variance': self.series.var(),
-                        'iqr': self.series.quantile(0.75) - self.series.quantile(0.25),
-                        'kurtosis': self.series.kurt(),
-                        'skewness': self.series.skew(),
-                        'sum': self.series.sum(),
-                        'mad': self.series.mad(),
-                        'cv': self.series.std() / self.series.mean(),
-                        'n_zeros': n_zeros,
-                        'p_zeros': n_zeros / self.series.size
-                    })
+                description += [
+                    ('{:.0%}'.format(perc), series.quantile(perc))
+                        for perc in [0.05, 0.25, 0.5, 0.75, 0.95]
+                ]
 
-        return description
+                if dtype == constants.TYPE_NUM:
+                    n_zeros = series.size - np.count_nonzero(series)
+
+                    description += [
+                        ('mean', series.mean()),
+                        ('std', series.std()),
+                        ('variance', series.var()),
+                        ('iqr', series.quantile(0.75) - series.quantile(0.25)),
+                        ('kurtosis', series.kurt()),
+                        ('skewness', series.skew()),
+                        ('sum', series.sum()),
+                        ('mad', series.mad()),
+                        ('cv', series.std() / series.mean()),
+                        ('n_zeros', n_zeros),
+                        ('p_zeros', n_zeros / series.size)
+                    ]
+
+        return OrderedDict(description)
 
     # def get_frequent_values(self, top):
-    #     return self.series.value_counts(dropna=False).head(top)
+    #     return series.value_counts(dropna=False).head(top)
     #
     # def get_distribution(self, top):
-    #     assert self.dtype in [constants.STR, constants.DATE, constants.NUM], 'Distribution not available'
+    #     assert dtype in [constants.STR, constants.DATE, constants.NUM], 'Distribution not available'
     #
-    #     if self.dtype == constants.STR:
+    #     if dtype == constants.STR:
     #         # Histogram of most frequen top values
     #         pass
     #     else:
     #         # Distribution Plot
     #         pass
 
-# TODO: verbose option that returns all feature and geometry
 class GeoReport:
-    def __init__(self, df, **kwargs):
+    def __init__(self, df, verbose=False, **kwargs):
         self.df = df
+        self.df.index.name = 'index'
+
         self.description = {}
+
+        self.verbose = verbose
 
     @property
     def describe(self):
-        return pd.concat(
-                    self.description.values(),
-                    keys=self.description.keys()
-                ).reset_index().sort_values('level_1').set_index(['level_1', 'level_0'])
+        desc = pd.DataFrame()
+
+        if self.description:
+            desc = pd.concat(self.description.values(), keys=self.description.keys()).reset_index()
+            desc.rename({
+                'level_0': 'issue',
+                'level_1': 'index'
+            }, axis=1, inplace=True)
+
+            desc = desc.sort_values('index').set_index(['index', 'issue'])
+
+        if self.verbose:
+            desc = self.df.join(desc.drop('geometry', axis=1), how='inner')
+
+        return desc
+
+    @property
+    def introduce(self):
+        z = self.df.has_z.value_counts()
+
+        base = pd.Series({
+            'crs': self.df.crs['init'],
+            'single_geom_type': self.df.geom_type.nunique() == 1,
+            'total_bounds': self.df.total_bounds,
+            '3d_shapes': z[True] if True in z.index else 0
+        })
+
+        geom = self.df.geom_type.value_counts()
+
+        return base.append(geom).to_frame(name='values')
 
     def find_invalids(self):
         invalid = gpd.GeoDataFrame(self.df[~self.df.is_valid]['geometry'])
@@ -143,14 +182,8 @@ class GeoReport:
 
             return invalid
 
-    # TODO: introduce data function (similar to DataReport)
-    # def validate_single_geom_type(data):
-    #     geom_types = data.geom_type.unique()
-    #
-    #     return len(geom_types) == 1
-
     def find_outsiders(self, xmin, xmax, ymin, ymax):
-        # TODO: assert bbox
+        assert xmin < xmax and ymin < ymax, 'Invalid bounding box'
 
         # There has to be a better way to select outside of a bounding box...
         invalid = gpd.GeoDataFrame(self.df.loc[~self.df.index.isin(self.df.cx[xmin:xmax, ymin:ymax].index)]['geometry'])
