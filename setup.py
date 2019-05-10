97ea823a57454d3820972a020fd4911c425f5ee5
dottyz <yizhao.tan@gmail.com>
2019-05-10 13:34:15 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='petk',
-    version='0.0.1',
+    version='0.0.2',
     author='Open Data Toronto',
     author_email='opendata@toronto.ca',
     packages=['petk'],
